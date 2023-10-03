6910fceb3612a4546b85faf421767f62496ac86a
Saba <khubashvili.saba12@gmail.com>
2023-10-03 23:55:59 +0400

    fix:fixed metadata

--- components/Layouts/HeaderLayout.tsx
+++ components/Layouts/HeaderLayout.tsx
@@ -16,22 +16,22 @@ export const HeaderLayout = ({ children }: Props) => {
 
         <meta property="og:url" content="https://suprimo.netlify.app" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="{children}" />
+        <meta property="og:title"  content={`${children}` || ''} />
         <meta
           property="og:description"
           content="Explore the limitless possibilities of AI with GPT-3! Our website is your gateway to the world of cutting-edge artificial intelligence. Discover how GPT-3 can assist you in various applications, from natural language understanding to content generation. Stay updated on the latest advancements in AI and harness the power of GPT-3 for your projects and innovations."
         />
-        <meta property="og:image" content="{MetadataImage}" />
+        <meta property="og:image" content={"@/public/assets/Images/FeaturedSecond.webp"} />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="suprimo.netlify.app" />
         <meta property="twitter:url" content="https://suprimo.netlify.app" />
-        <meta name="twitter:title" content="{children}" />
+        <meta name="twitter:title" content={`${children}` || ''} />
         <meta
           name="twitter:description"
           content="Explore the limitless possibilities of AI with GPT-3! Our website is your gateway to the world of cutting-edge artificial intelligence. Discover how GPT-3 can assist you in various applications, from natural language understanding to content generation. Stay updated on the latest advancements in AI and harness the power of GPT-3 for your projects and innovations."
         />
-        <meta name="twitter:image" content="{MetadataImage}" />
+        <meta name="twitter:image" content={"@/public/assets/Images/FeaturedSecond.webp"} />
       </Head>
     </>
   );
