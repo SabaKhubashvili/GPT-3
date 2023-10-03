1300fe89920f2ad660c39442c2d2731c921dd026
Saba <khubashvili.saba12@gmail.com>
2023-10-04 00:02:23 +0400

    fix:fixed metadata

--- components/Layouts/HeaderLayout.tsx
+++ components/Layouts/HeaderLayout.tsx
@@ -20,7 +20,7 @@ export const HeaderLayout = ({ children }: Props) => {
           property="og:description"
           content="Explore the limitless possibilities of AI with GPT-3! Our website is your gateway to the world of cutting-edge artificial intelligence. Discover how GPT-3 can assist you in various applications, from natural language understanding to content generation. Stay updated on the latest advancements in AI and harness the power of GPT-3 for your projects and innovations."
         />
-        <meta property="og:image" content={"https://www.ibm.com/blog/wp-content/uploads/2023/03/What-is-Generative-AI-what-are-Foundation-Models-and-why-do-they-matter-scaled.jpg"} />
+        <meta property="og:image" content={"https://www.mckinsey.com/~/media/mckinsey/business%20functions/mckinsey%20digital/our%20insights/the%20economic%20potential%20of%20generative%20ai%20the%20next%20productivity%20frontier/the-economic-potential-of-generative-ai-1324915617-thumb-1536x1536.jpg?mw=677&car=42:25"} />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="suprimo.netlify.app" />
@@ -30,7 +30,7 @@ export const HeaderLayout = ({ children }: Props) => {
           name="twitter:description"
           content="Explore the limitless possibilities of AI with GPT-3! Our website is your gateway to the world of cutting-edge artificial intelligence. Discover how GPT-3 can assist you in various applications, from natural language understanding to content generation. Stay updated on the latest advancements in AI and harness the power of GPT-3 for your projects and innovations."
         />
-        <meta name="twitter:image" content={"https://www.ibm.com/blog/wp-content/uploads/2023/03/What-is-Generative-AI-what-are-Foundation-Models-and-why-do-they-matter-scaled.jpg"} />
+        <meta name="twitter:image" content={"https://www.mckinsey.com/~/media/mckinsey/business%20functions/mckinsey%20digital/our%20insights/the%20economic%20potential%20of%20generative%20ai%20the%20next%20productivity%20frontier/the-economic-potential-of-generative-ai-1324915617-thumb-1536x1536.jpg?mw=677&car=42:25"} />
       </Head>
     </>
   );
