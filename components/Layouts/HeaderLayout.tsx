80dcf93a25a54f2463415b588ef254eb770c12a8
Saba <khubashvili.saba12@gmail.com>
2023-10-03 23:59:29 +0400

    fix:fixed metadata

--- components/Layouts/HeaderLayout.tsx
+++ components/Layouts/HeaderLayout.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import MetadataImage from "@/public/assets/Images/FeaturedSecond.webp";
 interface Props {
   children: React.ReactNode;
 }
@@ -21,7 +20,7 @@ export const HeaderLayout = ({ children }: Props) => {
           property="og:description"
           content="Explore the limitless possibilities of AI with GPT-3! Our website is your gateway to the world of cutting-edge artificial intelligence. Discover how GPT-3 can assist you in various applications, from natural language understanding to content generation. Stay updated on the latest advancements in AI and harness the power of GPT-3 for your projects and innovations."
         />
-        <meta property="og:image" content={"@/public/assets/Images/FeaturedSecond.webp"} />
+        <meta property="og:image" content={"https://www.ibm.com/blog/wp-content/uploads/2023/03/What-is-Generative-AI-what-are-Foundation-Models-and-why-do-they-matter-scaled.jpg"} />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="suprimo.netlify.app" />
@@ -31,7 +30,7 @@ export const HeaderLayout = ({ children }: Props) => {
           name="twitter:description"
           content="Explore the limitless possibilities of AI with GPT-3! Our website is your gateway to the world of cutting-edge artificial intelligence. Discover how GPT-3 can assist you in various applications, from natural language understanding to content generation. Stay updated on the latest advancements in AI and harness the power of GPT-3 for your projects and innovations."
         />
-        <meta name="twitter:image" content={"@/public/assets/Images/FeaturedSecond.webp"} />
+        <meta name="twitter:image" content={"https://www.ibm.com/blog/wp-content/uploads/2023/03/What-is-Generative-AI-what-are-Foundation-Models-and-why-do-they-matter-scaled.jpg"} />
       </Head>
     </>
   );
